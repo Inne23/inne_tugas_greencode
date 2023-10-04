3a173b46552951f9da52f895bdda2ef0b3baf16f
Inne23 <innefebryanda@gmail.com>
2023-10-04 16:30:29 +0700

    add.tugas1

---
+++ tugas-1/soal1.js
@@ -0,0 +1,17 @@
+let nama = "innefebryanda"
+let usia = 20
+let tinggi = 150
+
+let bilangan1 = 23
+let bilangan2 = 5
+let bilangan3 = 3
+
+let isstudent= true
+let isstudent1= false
+
+console.log("hasilpenjumlahan=", bilangan1+ bilangan2)
+console.log("hasilpengurangan=", bilangan2 - bilangan3)
+console.log("usia dan tinggi=", usia *  tinggi )
+console.log(isstudent)
+console.log(isstudent1)
+

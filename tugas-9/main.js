dac7d7f4f192f632a63dd5f8108e30804016c26a
Inne23 <innefebryanda@gmail.com>
2023-11-29 11:40:17 +0700

    tugas 9

---
+++ tugas-9/main.js
@@ -0,0 +1,79 @@
+// ini soal nmr 1
+let panjang = 25
+let lebar = 20
+let perkalian = panjang * lebar
+
+console.log(perkalian)
+
+// ini soal nmr 2
+let angkapertama = 2
+let angkakedua = 8
+let hasil = angkapertama + angkakedua
+let ratarata =  hasil / 2
+console.log(ratarata)
+
+// ini soal nmr 3
+let alas = 23
+let tinggi = 21
+function hitungluassegitiga(a, t) {
+    return 0.5 * a * t
+}
+let perkalian1= hitungluassegitiga( alas, tinggi)
+console.log(perkalian1)
+
+// ini soal nmr 4
+
+function hitungSisaBagi(angka1, angka2) {
+    return angka1 % angka2;
+}
+let sisabagi= hitungSisaBagi(10,3)
+console.log(sisabagi)
+
+// ini soal nmr 5
+
+function konversiDetikKeMenitDetik(detik) {
+    return detik/60
+}  
+let detikmenit =konversiDetikKeMenitDetik(80)
+console.log(detikmenit)
+
+// ini soal nmr 6
+
+function hitungKelilingPersegi(sisi) {
+    return 4 * sisi 
+}
+let keliling = hitungKelilingPersegi(8)
+console.log(keliling)
+
+// ini soal nmr 7
+let tugas1 = 35 
+let tugas2 = 30
+let waktu = tugas1 + tugas2
+
+console.log(waktu)
+
+// ini soal nmr 8
+
+function hitungVolumeSilinder(jariJari, tinggi) {
+    return 3,14*(jariJari* 2) * tinggi  
+}
+let volume = hitungVolumeSilinder(12, 4);
+  console.log(volume)
+
+
+//   ini soal nmr 9
+let angkasatu = 3
+let angkadua = 5
+let angkatiga = 8
+let total = angkasatu + angkadua + angkatiga;
+let rataRata = total / 3;
+console.log(rataRata);
+
+// ini soal nmr 10
+let satu = 30
+let dua = 12
+let tiga = 23
+let jumlah = satu + dua + tiga;
+let rata = jumlah / 3;
+console.log(rata);
+

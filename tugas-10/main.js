4cfa43a2bac4d78ef6a6015077fef8fe995eae7f
Inne23 <innefebryanda@gmail.com>
2023-12-06 12:29:46 +0700

    tugas-11

---
+++ tugas-10/main.js
@@ -0,0 +1,69 @@
+// ini soal nmr 1
+const angka = 7;
+if (angka % 2 == 0) {
+    console.log(angka, "bilangan genap")
+    
+}else{
+    console.log(angka, "bilangan ganjil")
+}
+
+
+// ini soal nmr 2
+const Angka1 = 12
+const Angka2 = 8
+if (Angka1 > Angka2) {
+    console.log (" lebih besar dari ")
+    
+}else if (Angka1 < Angka2) {
+    console.log("angka lebih kecil ")
+    
+}
+
+// ini soal nmr 3
+const kata = "inne"
+if (kata > 5) {
+    console.log(" memiliki lebih dari 5 karakter")
+} else {
+    console.log(" kurang dari 5 karakter ")
+}
+
+// ini soal nmr 4
+
+
+
+
+
+
+// ini soal nmr 5
+const berat = 5 
+if (berat > 5) {
+    console.log("biaya pengiriman 50000")
+}else {
+    console.log("biaya pengiriman 20000")
+}
+
+
+// ini soal nmr 6
+
+
+
+
+
+// ini soal nmr 7
+const Angka = 20;
+if (Angka % 2 == 0) {
+    console.log(Angka, "bilangan genap")
+    
+}else{
+    console.log(Angka, "bilangan ganjil")
+}
+
+
+// ini soal nmr 8
+const kalimat = "malam"
+if (kalimat = true) {
+    console.log("adalah palindrom") 
+    
+}else{
+    console.log(Angka, "bukan palindrom")
+}
---
+++ tugas-11/main.js
@@ -0,0 +1,28 @@
+for (let index = 1; index <= 5; index++) {
+    console.log(index)
+ }
+
+
+// ini nmr 2
+let hasil = 0
+let array = []
+for (let index = 1; index <= 10; index++) {
+  hasil += index
+  array.push(hasil)
+
+}console.log(array)
+
+
+// ini nmr 3
+for (let index = 5; index <= 50; index += 5) {
+    console.log(index);
+  }
+
+// ini nmr 4
+let genap = 0
+let arr = []
+for (let index = 2; index <= 10; index+=2) {
+  genap += index
+  arr.push(genap)
+     
+}console.log(arr) 
